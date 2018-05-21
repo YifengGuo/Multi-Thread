d01d9588a9c0fa94edbdb50154457f54ebdda29a
YifengGuo <gyf940517@gmail.com>
2018-05-20 21:57:24 -0600

    update

--- src/basic_java_thread/implementation_by_inheritance/MyThread.java
+++ src/basic_java_thread/implementation_by_inheritance/MyThread.java
@@ -27,17 +27,17 @@ public class MyThread extends Thread {
         MyThread thread3 = new MyThread(10);
 
         /*
-        By invoking run(), the run() method of its thread is invoked by main thread one by one
-        when former thread ends, the latter thread's run() will be invoked
+         * By invoking run(), the run() method of its thread is invoked by main thread one by one
+         * when former thread ends, the latter thread's run() will be invoked
          */
 //        thread1.run();
 //        thread2.run();
 //        thread3.run();
 
         /*
-        By invoking start(), each thread is executing concurrently.
-        There is no certain order which will ends first or last.
-        (thread1 - thread3 and main thread are all executing concurrently)
+         * By invoking start(), each thread is executing concurrently.
+         * There is no certain order which will ends first or last.
+         * (thread1 - thread3 and main thread are all executing concurrently)
          */
         thread1.start();
         thread2.start();
