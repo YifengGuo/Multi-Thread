db72fb0d867afdfad59619895e8f4bbe135dd12c
YifengGuo <gyf940517@gmail.com>
2019-04-22 15:17:47 +0800

    add busboy object

--- src/basic_java_thread/demo_thinking_in_java/communication_between_threads/Restaurant.java
+++ src/basic_java_thread/demo_thinking_in_java/communication_between_threads/Restaurant.java
@@ -15,16 +15,19 @@ import java.util.concurrent.TimeUnit;
  */
 public class Restaurant {
 
-    Meal meal;
+    volatile Meal meal;
 
     ExecutorService exec = Executors.newCachedThreadPool();
 
     WaitPerson person = new WaitPerson(this);
 
+    BusBoy busBoy = new BusBoy(this);
+
     Chef chef = new Chef(this);
 
     public Restaurant() {
         exec.submit(person);
+        exec.submit(busBoy);
         exec.submit(chef);
     }
 
@@ -48,9 +51,14 @@ class Meal {
 
 class WaitPerson implements Runnable {
     private Restaurant restaurant;
+    public boolean mealFinished;
 
     public WaitPerson(Restaurant restaurant) {
         this.restaurant = restaurant;
+    }
+
+    public void finishMeal() {
+        mealFinished = true;
     }
 
     @Override
@@ -58,22 +66,22 @@ class WaitPerson implements Runnable {
         try {
             while (!Thread.interrupted()) {
                 synchronized (this) {
-                    if (restaurant.meal == null) {
+                    if (restaurant.meal == null || !restaurant.chef.mealCooked || !restaurant.busBoy.isCleanedUp) {
                         wait();  // wait for the chef to cook the meal
                     }
                 }
 
-                TimeUnit.MILLISECONDS.sleep(100);  // take some time to eat
                 System.out.println("WaitPerson got " + restaurant.meal);
+                restaurant.busBoy.isCleanedUp = false;  // reset meal clean flag
+                finishMeal();
 
-                synchronized (restaurant.chef) {
-                    // notify that meal was consumed by the customer and chef can do next meal
-                    restaurant.meal = null;
-                    restaurant.chef.notifyAll();
+                synchronized (restaurant.busBoy) {
+                    // notify that meal was consumed by the customer and busBoy can clean the meal
+                    restaurant.busBoy.notify();
                 }
             }
         } catch (InterruptedException e) {
-            System.out.println("Exiting via interrupting...");
+            System.out.println("WaitPerson exiting via interrupting...");
         }
     }
 }
@@ -81,9 +89,14 @@ class WaitPerson implements Runnable {
 class Chef implements  Runnable {
     private Restaurant restaurant;
     private int count;  // count for current meal
+    public boolean mealCooked;
 
     public Chef(Restaurant restaurant) {
         this.restaurant = restaurant;
+    }
+
+    private void cookMeal() {
+        mealCooked = true;
     }
 
     @Override
@@ -104,14 +117,51 @@ class Chef implements  Runnable {
 
                 // notify customer to eat the meal
                 synchronized (restaurant.person) {
-                    TimeUnit.MILLISECONDS.sleep(100);  // take some time to cook the meal
                     restaurant.meal = new Meal(count++);
-                    System.out.println(restaurant.meal + " Finished.");
-                    restaurant.person.notifyAll();
+                    System.out.println(restaurant.meal + " cooked.");
+                    cookMeal();
+                    restaurant.person.notify();
                 }
             }
         } catch (InterruptedException e) {
-            System.out.println("Exiting via interrupting...");
+            System.out.println("Chef exiting via interrupting...");
         }
+    }
+}
+
+class BusBoy implements Runnable {
+    private Restaurant restaurant;
+
+    public boolean isCleanedUp;
+
+    public BusBoy(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                synchronized (this) {
+                    if (restaurant.meal == null || !restaurant.person.mealFinished) {
+                        wait(); // wait for the chef to cook the meal for the customer
+                    }
+                }
+
+                // notify the chef to cook the next meal
+                synchronized (restaurant.chef) {
+                    System.out.println(restaurant.meal + " is cleaned up");
+                    clearMeal();
+                    restaurant.chef.notify();
+                }
+            }
+        } catch (InterruptedException e) {
+            System.out.println("BusBoy exiting via interrupting...");
+        }
+    }
+
+    private void clearMeal() {
+        restaurant.meal = null;
+        isCleanedUp = true;
     }
 }
