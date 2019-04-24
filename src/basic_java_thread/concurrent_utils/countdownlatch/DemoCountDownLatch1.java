081d6a298261a277f59076d0ca8bd2d73677faf6
YifengGuo <gyf940517@gmail.com>
2019-04-24 11:28:24 +0800

    demo for CountDownLatch

---
+++ src/basic_java_thread/concurrent_utils/countdownlatch/DemoCountDownLatch1.java
@@ -0,0 +1,101 @@
+package basic_java_thread.concurrent_utils.countdownlatch;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by guoyifeng on 4/24/19
+ */
+
+/**
+ * A demo for one usage of CountDownLatch
+ * used under a situation when some tasks shall wait until
+ * some other tasks complete. CountDownLatch could be used
+ * to check if all prerequisites tasks are all complete or not
+ * when CountDownLatch counts equals zero, waiting tasks shall begin
+ */
+public class DemoCountDownLatch1 {
+    private static final int SIZE = 100;  // the count of prerequisites portions
+    private static final int WAITING_SIZE = 10;  // the count of waiting tasks
+    public static void main(String[] args) {
+        // All must share a single CountDownLatch object
+        CountDownLatch latch = new CountDownLatch(SIZE);
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        // initialize WaitingTask
+        for (int i = 0; i < WAITING_SIZE; i++) {
+            exec.submit(new WaitingTask(latch));
+        }
+
+        // initialize TaskPortion
+        for (int i = 0; i < SIZE; i++) {
+            exec.submit(new TaskPortion(latch));
+        }
+
+        System.out.println("All tasks launched");
+
+        exec.shutdown();  // use shutdown() instead of shutdownNow() to make execution complete before interruption
+    }
+}
+
+// performs some portion of a task
+class TaskPortion implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+    private Random rand = new Random(66);
+    private final CountDownLatch latch;
+
+    public TaskPortion(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+    public void run() {
+        try {
+            doWork();
+            latch.countDown();  // means a portion of a task is complete
+                                // when all portions of this task complete, other waiting tasks can be started
+        } catch (InterruptedException e) {
+            System.out.println(this + " exiting via interrupting...");
+        }
+    }
+
+    public void doWork() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(rand.nextInt(2000));
+        System.out.println(this + " completed");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%1$-3d ", id);
+    }
+}
+
+class WaitingTask implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+    private Random rand = new Random(66);
+    private final CountDownLatch latch;
+
+    public WaitingTask(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+    public void run() {
+        try {
+            latch.await();  // wait until all (prerequisites) portions done
+            System.out.println("Latch barrier passed for " + this);
+        } catch (InterruptedException e) {
+            System.out.println("WaitingTask "+ this +"exiting via interrupting");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%1$-3d ", id);
+    }
+}
---
+++ src/basic_java_thread/concurrent_utils/countdownlatch/DemoCountDownLatch2.java
@@ -0,0 +1,105 @@
+package basic_java_thread.concurrent_utils.countdownlatch;
+
+/**
+ * Created by guoyifeng on 4/24/19
+ */
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Another demo of using CountDownLatch
+ * Here is a pair of classes in which a group of worker threads use two countdown latches:
+ *
+ * The first is a start signal that prevents any worker from proceeding until the driver is ready for them to proceed;
+ * The second is a completion signal that allows the driver to wait until all workers have completed.
+ *
+ * Assume we have one driver and several workers
+ */
+public class DemoCountDownLatch2 {
+    private static final int WORKER_COUNT = 10;
+
+    public static void main(String[] args) {
+        CountDownLatch startSingal = new CountDownLatch(1);
+        CountDownLatch completeSingal = new CountDownLatch(WORKER_COUNT);
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        exec.submit(new Driver(startSingal, completeSingal));
+
+        for (int i = 0; i < WORKER_COUNT; ++i) {
+            exec.submit(new Worker(startSingal, completeSingal));
+        }
+
+        if (completeSingal.getCount() == 0) {
+            System.out.println("All job done");
+        }
+        exec.shutdown();
+    }
+}
+
+class Driver implements Runnable {
+    private final CountDownLatch startSignal;
+    private final CountDownLatch completeSignal;
+    private Random rand = new Random(67);
+
+    public Driver(CountDownLatch startSignal, CountDownLatch completeSignal) {
+        this.startSignal = startSignal;
+        this.completeSignal = completeSignal;
+    }
+
+    @Override
+    public void run() {
+        try {
+            doSomethingElse();
+            System.out.println("Driver job done");
+            startSignal.countDown(); // let all workers start working
+            completeSignal.await();  // waiting for all the workers complete job
+            System.out.println("All workers job done");
+        } catch (InterruptedException e) {
+            System.out.println("Driver exiting via interrupting");
+        }
+    }
+
+    public void doSomethingElse() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(rand.nextInt(2000));
+    }
+}
+
+class Worker implements Runnable {
+    private final CountDownLatch startSignal;
+    private final CountDownLatch completeSignal;
+    private static int counter = 0;
+    private final int id = counter++;
+    private Random rand = new Random(67);
+
+    public Worker(CountDownLatch startSignal, CountDownLatch completeSignal) {
+        this.startSignal = startSignal;
+        this.completeSignal = completeSignal;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // waiting for driver
+            startSignal.await();
+            doSomethingElse();  // worker working period
+            System.out.println("Worker " + this + " job done");
+            completeSignal.countDown();
+        } catch (InterruptedException e) {
+            System.out.println("Woker " + this + " exiting via interrupting");
+        }
+    }
+
+    public void doSomethingElse() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(rand.nextInt(5000));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%1$-3d ", id);
+    }
+}
