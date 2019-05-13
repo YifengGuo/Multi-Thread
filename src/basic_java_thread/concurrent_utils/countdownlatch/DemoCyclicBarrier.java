4189a2ad07ea362a4a2b9988aa66777c02a1673e
YifengGuo <gyf940517@gmail.com>
2019-05-13 15:31:20 +0800

    simple simulation for CyclicBarrier

---
+++ src/basic_java_thread/concurrent_utils/countdownlatch/DemoCyclicBarrier.java
@@ -0,0 +1,108 @@
+package basic_java_thread.concurrent_utils.countdownlatch;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * Created by guoyifeng on 4/26/19
+ */
+class Horse implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+    private int strides = 0;
+    private static Random random = new Random(47);
+    private static CyclicBarrier barrier;
+
+    Horse(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
+
+    public synchronized int getStrides() {
+        return strides;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                synchronized (this) {
+                    strides += random.nextInt(3);
+                }
+                barrier.await();  // waiting for other horses update its strides
+            }
+        } catch (InterruptedException e) {
+            // A legitimate way to exit
+        } catch (BrokenBarrierException e) {
+            // This one we want to know about
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Horse " + id + " ";
+    }
+
+    public String tracks() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < getStrides(); i++) {
+            sb.append("*");
+        }
+        sb.append(id);
+        return sb.toString();
+    }
+}
+
+public class DemoCyclicBarrier {
+    static final int FINISH_LINE = 75;
+    private List<Horse> horses = new ArrayList<>();
+    private ExecutorService exec = Executors.newCachedThreadPool();
+    private CyclicBarrier barrier;
+
+    public DemoCyclicBarrier(int nHorses, final int pause) {
+        barrier = new CyclicBarrier(nHorses, () -> {  // after all horses run(), do as below
+            StringBuilder s = new StringBuilder();
+            for (int i = 0; i < FINISH_LINE; i++) {  // separate from last round printed result
+                s.append("=");
+            }
+            System.out.println(s);
+            for (Horse horse : horses) {
+                System.out.println(horse.tracks());
+            }
+            for (Horse horse : horses) {
+                if (horse.getStrides() >= FINISH_LINE) {
+                    System.out.println(horse + " won!");
+                    exec.shutdownNow();
+                    return;
+                }
+            }
+            try {
+                TimeUnit.MILLISECONDS.sleep(pause);
+            } catch (InterruptedException e) {
+                System.out.println("barrier-action sleep interrupted");
+            }
+        });
+        for (int i = 0; i < nHorses; i++) {
+            Horse horse = new Horse(barrier);
+            horses.add(horse);
+            exec.execute(horse);
+        }
+    }
+
+    public static void main(String[] args) {
+        int nHorses = 7;
+        int pause = 200;
+        if (args.length > 0) {
+            int n = new Integer(args[0]);
+            nHorses = n > 0 ? n : nHorses;
+        }
+        if (args.length > 1) {
+            int p = new Integer(args[1]);
+            pause = p > -1 ? p : pause;
+        }
+
+        new DemoCyclicBarrier(nHorses, pause);
+    }
+}
