0723fd73bde1e170ec8901a0375bf2c16970b041
YifengGuo <gyf940517@gmail.com>
2019-05-14 14:04:29 +0800

    demo for other data structures in current utils

---
+++ src/basic_java_thread/concurrent_utils/countdownlatch/DemoDelayQueue.java
@@ -0,0 +1,120 @@
+package basic_java_thread.concurrent_utils.countdownlatch;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+/**
+ * Created by guoyifeng on 5/13/19
+ *
+ * DelayQueue:
+ * An unbounded blocking queue of Delayed elements, in which an element can only be taken when its delay has expired.
+ * The head of the queue is that Delayed element whose delay expired furthest in the past.  (its delay has been expired for the longest period)
+ * If no delay has expired there is no head and poll will return null.
+ * Expiration occurs when an element's getDelay(TimeUnit.NANOSECONDS) method returns a value less than or equal to zero.
+ * Even though unexpired elements cannot be removed using take or poll, they are otherwise treated as normal elements.
+ * For example, the size method returns the count of both expired and unexpired elements.
+ * This queue does not permit null elements.
+ */
+class DelayedTask implements Runnable, Delayed {
+    private static int counter = 0;
+    private final int id = counter++;
+    private final int delta;
+    private final long trigger;
+    protected static List<DelayedTask> sequence = new ArrayList<>();
+
+    DelayedTask(int delayInMilliseconds) {
+        this.delta = delayInMilliseconds;
+        this.trigger = System.nanoTime() +
+                NANOSECONDS.convert(delta, MILLISECONDS);
+        sequence.add(this);
+    }
+
+    @Override
+    public long getDelay(TimeUnit unit) {
+        return unit.convert(trigger - System.nanoTime(), NANOSECONDS);
+    }
+
+    @Override
+    public int compareTo(Delayed arg) {
+        DelayedTask that = (DelayedTask) arg;
+        if (trigger < that.trigger) {
+            return -1;
+        }
+        if (trigger > that.trigger) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public void run() {
+        System.out.print(this + " ");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%1$-4d]", delta) + " Task " + id;
+    }
+
+    public String summary() {
+        return "(" + id + ":" + delta + ")";
+    }
+
+    public static class EndSentinel extends DelayedTask {
+        private ExecutorService exec;
+
+        EndSentinel(int delay, ExecutorService e) {
+            super(delay);
+            this.exec = e;
+        }
+
+        @Override
+        public void run() {
+            for (DelayedTask pt : sequence) {
+                System.out.print(pt.summary() + " ");
+            }
+            System.out.println();
+            System.out.println(this + " Calling shutdownNow()");
+            exec.shutdownNow();
+        }
+    }
+}
+
+class DelayedTaskConsumer implements Runnable {
+    private DelayQueue<DelayedTask> q;
+
+    DelayedTaskConsumer(DelayQueue<DelayedTask> q) {
+        this.q = q;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                q.take().run();
+            }
+        } catch (InterruptedException e) {
+            // Acceptable way to exit
+        }
+        System.out.println("Finished DelayedTaskConsumer ");
+    }
+}
+
+public class DemoDelayQueue {
+    public static void main(String[] args) {
+        Random random = new Random(47);
+        ExecutorService exec = Executors.newCachedThreadPool();
+        DelayQueue<DelayedTask> queue = new DelayQueue<DelayedTask>();
+        for (int i = 0; i < 20; i++) {
+            queue.put(new DelayedTask(random.nextInt(5000)));
+        }
+        queue.add(new DelayedTask.EndSentinel(5000, exec));
+        exec.execute(new DelayedTaskConsumer(queue));
+    }
+}
